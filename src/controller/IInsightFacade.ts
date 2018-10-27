24cacb978c7568573c9dafdd0a2e63bc7e7b8aa5
Wesley <wesley.sequeira@hotmail.com>
2018-10-26 17:13:37 -0700

    CourseSaver in IInsightFacade

--- src/controller/IInsightFacade.ts
+++ src/controller/IInsightFacade.ts
@@ -8,6 +8,31 @@ export enum InsightDatasetKind {
     Courses = "courses",
     Rooms = "rooms",
 }
+export class CourseSaver {
+    public dept: string;
+    public id: string;
+    public avg: number;
+    public instructor: string;
+    public title: string;
+    public pass: number;
+    public fail: number;
+    public audit: number;
+    public uuid: string;
+    public year: number;
+
+    constructor(dept: string, id: string, avg: number, instructor: string, title: string, pass: number, fail: number, audit: number, uuid: string, year: number) {
+        this.dept = dept;
+        this.id = id;
+        this.avg = avg;
+        this.instructor = instructor;
+        this.title = title;
+        this.pass = pass;
+        this.fail = fail;
+        this.audit = audit;
+        this.uuid = uuid;
+        this.year = year;
+    }
+}
 
 export interface InsightDataset {
     id: string;
@@ -21,7 +46,6 @@ export class InsightError extends Error {
         Error.captureStackTrace(this, InsightError);
     }
 }
-
 export class NotFoundError extends Error {
     constructor(...args: any[]) {
         super(...args);
--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,5 +1,5 @@
 import Log from "../Util";
-import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
+import {IInsightFacade, InsightDataset, InsightDatasetKind, CourseSaver} from "./IInsightFacade";
 import {InsightError, NotFoundError} from "./IInsightFacade";
 import * as JSZip from "jszip";
 import {getRelativePath} from "tslint/lib/configuration";
@@ -13,31 +13,6 @@ import * as Path from "path";
  * Method documentation is in IInsightFacade
  *
  */
-export class CourseSaver {
-    public dept: string;
-    public id: string;
-    public avg: number;
-    public instructor: string;
-    public title: string;
-    public pass: number;
-    public fail: number;
-    public audit: number;
-    public uuid: string;
-    public year: number;
-
-    constructor(dept: string, id: string, avg: number, instructor: string, title: string, pass: number, fail: number, audit: number, uuid: string, year: number) {
-        this.dept = dept;
-        this.id = id;
-        this.avg = avg;
-        this.instructor = instructor;
-        this.title = title;
-        this.pass = pass;
-        this.fail = fail;
-        this.audit = audit;
-        this.uuid = uuid;
-        this.year = year;
-    }
-}
 
 export default class InsightFacade implements IInsightFacade {
     private coursesMap: Map<string, any>;
